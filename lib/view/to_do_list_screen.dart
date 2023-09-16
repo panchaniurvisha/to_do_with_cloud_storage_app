120e884125575af29354f757a3d564826a40a037
pills <urvisha1109@gmail.com>
2023-09-16 10:42:27 +0530

    "9/16/2023"

--- lib/view/to_do_list_screen.dart
+++ lib/view/to_do_list_screen.dart
@@ -18,124 +18,137 @@ class _ToDoListScreenState extends State<ToDoListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            AppString.title,
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.calendar_month_outlined))
-          ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          AppString.title,
         ),
-        body: StreamBuilder<QuerySnapshot>(
-          stream: users.snapshots(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return const Center(child: Text('Something went wrong'));
-            }
-            if (snapshot.hasData && snapshot.data!.docs.isEmpty) {
-              return const Center(child: Text("Document does not exist"));
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            return ListView.separated(
-              physics: const BouncingScrollPhysics(),
-              shrinkWrap: true,
-              separatorBuilder: (context, index) => SizedBox(
-                height: height(context) / 40,
-              ),
-              padding: EdgeInsets.symmetric(horizontal: width(context) / 20),
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                Map<String, dynamic> data =
-                    snapshot.data!.docs[index].data() as Map<String, dynamic>;
-                DocumentSnapshot taskSnapshot = snapshot.data!.docs[index];
-                return ListTile(
-                  title: Text(data["task"]),
-                  trailing: PopupMenuButton<int>(
-                    itemBuilder: (context) => [
-                      const PopupMenuItem(
-                        value: 1,
-                        child: Row(
-                          children: [
-                            Icon(Icons.edit),
-                            SizedBox(width: 10),
-                            Text("Edit")
-                          ],
-                        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.calendar_month_outlined),
+          ),
+        ],
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: users.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          }
+          if (snapshot.hasData && snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text("Document does not exist"));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            shrinkWrap: true,
+            separatorBuilder: (context, index) => SizedBox(
+              height: height(context) / 40,
+            ),
+            padding: EdgeInsets.symmetric(horizontal: width(context) / 20),
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              Map<String, dynamic> data = snapshot.data!.docs[index].data() as Map<String, dynamic>;
+              DocumentSnapshot taskSnapshot = snapshot.data!.docs[index];
+
+              debugPrint(taskSnapshot.id);
+              return ListTile(
+                title: Text(data["task"]),
+                trailing: PopupMenuButton<int>(
+                  itemBuilder: (context) => [
+                    const PopupMenuItem(
+                      value: 1,
+                      child: Row(
+                        children: [
+                          Icon(Icons.edit),
+                          SizedBox(width: 10),
+                          Text("Edit"),
+                        ],
                       ),
-                      PopupMenuItem(
-                          value: 65,
-                          child: const Row(
-                            children: [
-                              Icon(Icons.delete),
-                              SizedBox(width: 10),
-                              Text("Delete")
-                            ],
+                    ),
+                    PopupMenuItem(
+                      value: 65,
+                      child: const Row(
+                        children: [
+                          Icon(Icons.delete),
+                          SizedBox(width: 10),
+                          Text("Delete"),
+                        ],
+                      ),
+                      onTap: () => setState(
+                            () {
+                          deleteUser(taskSnapshot.id);
+                        },
+                      ),
+                    ),
+                  ],
+                  offset: const Offset(100, 40),
+                  color: Colors.grey,
+                  elevation: 10,
+                  onSelected: (value) {
+                    if (value == 1) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AddEditTaskScreen(
+                            index: index,
+                            taskData: data,
+                            id: taskSnapshot.id,
                           ),
-                          onTap: () => setState(() {
-                                deleteUser(taskSnapshot.id);
-                              })),
-                    ],
-                    offset: const Offset(100, 40),
-                    color: Colors.grey,
-                    elevation: 10,
-                    onSelected: (value) {
-                      if (value == 1) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => AddEditTaskScreen(
-                                    index: index,
-                                    taskData: data))).then((value) {
-                          setState(() {});
-                        });
-                      }
-                      debugPrint(value.toString());
-                    },
+                        ),
+                      ).then((value) {
+                        setState(() {});
+                      });
+                    }
+                    debugPrint(value.toString());
+                  },
+                ),
+                subtitle: Text(data["description"]),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(width(context) / 20),
+                  side: const BorderSide(
+                    color: Colors.black,
                   ),
-                  subtitle: Text(data["addTaskTag"] + "" + data["description"]),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(width(context) / 20),
-                      side: const BorderSide(color: Colors.black)),
-                );
-              },
-            );
-          },
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(width(context) / 10),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(width(context) / 10),
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const AddEditTaskScreen(),
           ),
-          onPressed: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const AddEditTaskScreen())),
-          child: const Icon(Icons.add),
         ),
-        bottomNavigationBar:
-            BottomNavigationBar(items: const <BottomNavigationBarItem>[
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.square_list), label: ""),
           BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.square_list), label: ""),
-          BottomNavigationBarItem(
-              icon: Icon(
-                CupertinoIcons.tag,
-              ),
-              label: "")
-        ]));
+            icon: Icon(
+              CupertinoIcons.tag,
+            ),
+            label: "",
+          ),
+        ],
+      ),
+    );
   }
 
   editData() {}
   Future<void> deleteUser(String taskId) {
-    return users
-        .doc(taskId)
-        .delete()
-        .then((value) => debugPrint("User Deleted"))
-        .catchError((error) => debugPrint("Failed to delete user: $error"));
+    return users.doc(taskId).delete().then((value) => debugPrint("User Deleted")).catchError(
+          (error) => debugPrint("Failed to delete user: $error"),
+    );
   }
 }
